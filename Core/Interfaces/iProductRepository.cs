7601a394b4d1fb0e347b3ec222a23b43cb94364c
Charles Showalter <cshowalter@xtechnology.org>
2022-05-10 10:26:43 -0700

    Added Brand and Types to Controller

--- Core/Interfaces/iProductRepository.cs
+++ Core/Interfaces/iProductRepository.cs
@@ -6,5 +6,7 @@ namespace Core.Interfaces
     {
         Task<Product> GetProductByIdAsync(int id);
         Task<IReadOnlyList<Product>> GetProductsAync();
+        Task<IReadOnlyList<ProductBrand>> GetProductBrandsAsync();
+        Task<IReadOnlyList<ProductType>> GetProductTypesAsync();
     }
 }
