b1884450ca9d629a955d6936a3c29fedf912732c
Charles Showalter <cshowalter@xtechnology.org>
2022-05-13 16:58:42 -0700

    Added Toastr

---
+++ client/src/app/core/test-error/test-error.component.ts
@@ -0,0 +1,58 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { environment } from 'src/environments/environment';
+
+@Component({
+  selector: 'app-test-error',
+  templateUrl: './test-error.component.html',
+  styleUrls: ['./test-error.component.scss']
+})
+export class TestErrorComponent implements OnInit {
+  baseURL = environment.apiUrl;
+
+  constructor(private http: HttpClient) { }
+
+  ngOnInit(): void {
+  }
+
+  get404Error(){
+    this.http.get(this.baseURL + 'products/42').subscribe(
+      {
+        next: (response) => { console.log(response); },
+        error: (e: any) => { console.log(e); },
+        complete: () => { console.log('complete'); }
+      }
+    );
+  }
+
+  get500Error(){
+    this.http.get(this.baseURL + 'buggy/servererror').subscribe(
+      {
+        next: (response) => { console.log(response); },
+        error: (e: any) => { console.log(e); },
+        complete: () => { console.log('complete'); }
+      }
+    );
+  }
+
+  get400Error(){
+    this.http.get(this.baseURL + 'buggy/badrequest').subscribe(
+      {
+        next: (response) => { console.log(response); },
+        error: (e: any) => { console.log(e); },
+        complete: () => { console.log('complete'); }
+      }
+    );
+  }
+
+  get400ValidationError(){
+    this.http.get(this.baseURL + 'products/five').subscribe(
+      {
+        next: (response) => { console.log(response); },
+        error: (e: any) => { console.log(e); },
+        complete: () => { console.log('complete'); }
+      }
+    );
+  }
+
+}
