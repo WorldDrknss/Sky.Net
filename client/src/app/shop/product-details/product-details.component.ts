f3ef7e326eb0a4236b37abc5f93114b20e48cfd3
Charles Showalter <cshowalter@xtechnology.org>
2022-05-17 11:59:02 -0700

    Change Theme, Animations, and Loading Spinners

--- client/src/app/shop/product-details/product-details.component.ts
+++ client/src/app/shop/product-details/product-details.component.ts
@@ -12,7 +12,9 @@ import { ShopService } from '../shop.service';
 export class ProductDetailsComponent implements OnInit {
   product: IProduct;
 
-  constructor(private shopService: ShopService, private activatedRoute: ActivatedRoute, private bcService: BreadcrumbService) { }
+  constructor(private shopService: ShopService, private activatedRoute: ActivatedRoute, private bcService: BreadcrumbService) {
+    this.bcService.set('@productDetails', ' ');
+   }
 
   ngOnInit(): void {
     this.loadProduct();
