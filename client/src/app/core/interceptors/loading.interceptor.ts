5fbfb8efe54f2f19d0f74da3ac6fcdc52435e4d5
Charles Showalter <cshowalter@xtechnology.org>
2022-05-31 17:49:04 -0700

    Removed intercepter for DELETE

--- client/src/app/core/interceptors/loading.interceptor.ts
+++ client/src/app/core/interceptors/loading.interceptor.ts
@@ -12,6 +12,10 @@ export class LoadingInterceptor implements HttpInterceptor {
       return next.handle(req);
     }
 
+    if(req.method === 'DELETE'){
+      return next.handle(req);
+    }
+
     if(req.url.includes('emailexists')){
       return next.handle(req);
     }
